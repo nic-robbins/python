2509bd759ea525463e5a7513c5f72e3ee1e2bb49
Nic <158803059+nic-robbins@users.noreply.github.com>
2026-07-21 23:10:56 -0500

    Create test_television.py

---
+++ test_television.py
@@ -0,0 +1,44 @@
+import pytest
+from television import *
+
+class Test:
+    def setup_method(self):
+        self.tv1 = Television()
+
+    def teardown_method(self):
+        del self.tv1
+
+    def test_init(self):
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_power(self):
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+    
+    def test_mute(self):
+        pass
+
+    def test_channel_up(self):
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+        self.tv1.power()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 1, Volume = 0'
+
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        self.tv1.channel_up()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+    def test_channel_down(self):
+        pass
+
+    def test_volume_up(self):
+        pass
+    
+    def test_volume_down(self):
+        pass
